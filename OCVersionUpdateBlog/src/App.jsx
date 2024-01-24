8226a21812cf138dd8838976488c0c3183f18657
Praxis Financial Group LLC <claimsteam@praxisassetrecovery.org>
2024-01-23 21:54:16 -0500

    Create Routes

--- OCVersionUpdateBlog/src/App.jsx
+++ OCVersionUpdateBlog/src/App.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import oclogohoriz from './assets/oclogohoriz.png'
 import oclogovert from './assets/oclogovert.png'
+import Home from './assets/pages/Home'
+import About from './assets/pages/About'
+import SignIn from './assets/pages/SignIn'
+import SignUp from './assets/pages/SignUp'
+import AppReleases from './assets/pages/AppReleases'
+import Dashboard from './assets/pages/Dashboard'
 export default function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <Routes> 
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />     
+          <Route path="/Sign-in" element={<SignIn/>} />
+          <Route path="/Sign-Up" element={<SignUp/>} />
+          <Route path="/App-Releases" element={<AppReleases/>} />
+          <Route path="/Dashboard" element={<Dashboard/>} />
+          
+          
+          
+      </Routes>
        
-          <a href="/Users/oortcloudenzo/Desktop/MERN-OCUpdateBlog/OCVersionUpdateBlog/src/assets/oclogohoriz.png" 
-          target="_blank" ><img src={oclogohoriz} className="CompanyLogo" alt="OcLogo"/>
-          </a>
-          <a href="/Users/oortcloudenzo/Desktop/MERN-OCUpdateBlog/OCVersionUpdateBlog/src/assets/oclogovert.png"
-          target="_blank"><img src={oclogovert} className="CommLogo" alt="CommercialLogo"/></a>
-          <h1 className='text-3xl text-blue-500'>OortCloud Lendsoft® </h1>
-      </div>
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+
+     </BrowserRouter>
     
+  
 
   )
 }
---
+++ OCVersionUpdateBlog/src/assets/pages/AppReleases.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AppReleases() {
+  return (
+    <div>AppReleases</div>
+  )
+}
